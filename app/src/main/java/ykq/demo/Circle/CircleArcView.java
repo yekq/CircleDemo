f0cb30a9db7896f7e78ddab4b34727b1df9d4940
yekq <yekangqi@gmail.com>
2016-11-30 16:53:54 +0800

    2016-11-30

--- app/src/main/java/ykq/demo/Circle/CircleArcView.java
+++ app/src/main/java/ykq/demo/Circle/CircleArcView.java
@@ -21,9 +21,9 @@ public class CircleArcView extends View{
     private float strokeWidth=20;//外环直径
     private RectF strokeRectF;//外环画板
     private float endAngle=360;//环形总度数
-    private float centreCircleRadius;//内圆半径
     private float process;//1~100
     private float scale=1f;//0~1;
+    private float maxProcess=100;//最大进度
     public CircleArcView(Context context) {
         super(context);
         init(context,null);
@@ -50,6 +50,11 @@ public class CircleArcView extends View{
         paint=new Paint();
         paint.setAntiAlias(true);
         paint.setStrokeWidth(strokeWidth);
+        strokeRectF=new RectF();
+    }
+
+    public void setMaxProcess(float maxProcess) {
+        this.maxProcess = maxProcess;
     }
 
     public void setProcess(float process)
@@ -64,32 +69,30 @@ public class CircleArcView extends View{
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        paint.setColor(Color.BLUE);
-        paint.setStyle(Paint.Style.STROKE);//半径内包含一半stroke
 
-        //外环
         float r=Math.min(getWidth(),getHeight());
-        if (centreCircleRadius==0 && r>0)
-        {
-            centreCircleRadius=r-strokeWidth;
-            strokeRectF=new RectF();
-        }
-        float tempPro=process/100f;
-        strokeRectF.set(strokeWidth/2f+(1f-scale)*r/2,strokeWidth/2f+(1f-scale)*r/2, (1+scale)*r/2-strokeWidth/2f,  (1+scale)*r/2-strokeWidth/2f);//包含一半stroke(要考虑边距)
-        canvas.drawArc(strokeRectF, 0, endAngle*tempPro, false, paint);//弧形所在矩形区域、起始角度、圆弧扫过的角度(顺时针方向，单位为度,从右中间开始为零度)、是否包括圆心（是：扇形圆弧；否：外层圆弧）、画笔
-//        paint.setColor(Color.BLACK);
-//        paint.setStyle(Paint.Style.FILL);
-//        paint.setStrokeWidth(2f);
-//        canvas.drawRect(strokeRectF,paint);
+
         //内圆
         paint.setColor(Color.RED);
+//        paint.setAlpha(50);
         paint.setStyle(Paint.Style.FILL);
-        float radius=centreCircleRadius*tempPro;
-        float maxRadius=r/2f;
-        if (radius>maxRadius)
-        {
-            radius=maxRadius;
-        }
-        canvas.drawCircle(r/2,r/2,radius*scale,paint);
+        float radius= r/2*scale-strokeWidth;
+        canvas.drawCircle(r/2,r/2,radius,paint);
+
+        //外环
+        paint.setColor(Color.BLUE);
+        paint.setStyle(Paint.Style.STROKE);//半径内包含一半stroke
+        paint.setStrokeWidth(strokeWidth);
+//        paint.setAlpha(255);
+        float tempPro=process/maxProcess;
+//        strokeRectF.set(strokeWidth*scale+(1f-scale)*r/2,strokeWidth*scale+(1f-scale)*r/2, (1+scale)*r/2-strokeWidth*scale,  (1+scale)*r/2-strokeWidth*scale);//包含一半stroke(要考虑边距)
+        strokeRectF.set(r/2-(r/2-strokeWidth/2)*scale,r/2-(r/2-strokeWidth/2)*scale,r/2+(r/2-strokeWidth/2)*scale,r/2+(r/2-strokeWidth/2)*scale);
+        canvas.drawArc(strokeRectF, 0, endAngle*tempPro, false, paint);//弧形所在矩形区域、起始角度、圆弧扫过的角度(顺时针方向，单位为度,从右中间开始为零度)、是否包括圆心（是：扇形圆弧；否：外层圆弧）、画笔
+
+//        paint.setColor(Color.GREEN);
+//        paint.setAlpha(50);
+//        paint.setStyle(Paint.Style.FILL);
+//        canvas.drawRect(strokeRectF,paint);
+
     }
 }
--- app/src/main/java/ykq/demo/Circle/MainActivity.java
+++ app/src/main/java/ykq/demo/Circle/MainActivity.java
@@ -1,15 +1,65 @@
 package ykq.demo.Circle;
 
+import android.animation.AnimatorSet;
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.content.res.Configuration;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
 
 public class MainActivity extends AppCompatActivity {
     private CircleArcView arcView;
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Log.d("test","onConfigurationChanged(Configuration newConfig)");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        Log.d("test","onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState)");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d("test","onRestoreInstanceState(Bundle savedInstanceState)");
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onRestoreInstanceState(savedInstanceState, persistentState);
+        Log.d("test","onRestoreInstanceState(Bundle savedInstanceState, PersistableBundle persistentState)");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("test","onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("test","onStop");
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("test","onCreate");
         setContentView(R.layout.activity_main);
         arcView= (CircleArcView) findViewById(R.id.arcView);
+
 //        ValueAnimator animator= ObjectAnimator.ofFloat(20,100);
 //        animator.setDuration(1000*4);
 //        animator.setInterpolator(new AccelerateDecelerateInterpolator());
@@ -24,19 +74,83 @@ public class MainActivity extends AppCompatActivity {
 //        });
 //        animator.start();
 
-//        Log.d("test",(a==b?"true":"false"));
-        arcView.setProcess(50);
-        arcView.setScale(0.5f);
-//        ObjectAnimator objectAnimator=ObjectAnimator.ofFloat(arcView,"scale",1,100);
-//        objectAnimator.setDuration(1000*4);
-//        objectAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-//            @Override
-//            public void onAnimationUpdate(ValueAnimator animation) {
-//                arcView.setProcess((Float) animation.getAnimatedValue());
-//                arcView.postInvalidate();
-//                Log.d("test",animation.getAnimatedValue().toString());
-//            }
-//        });
-//        objectAnimator.start();
+        testAnminationSet();
+        arcView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                testAnminationSet();
+            }
+        });
+    }
+
+    private void startAnimation()
+    {
+        final float maxProcess=360f;
+        arcView.setMaxProcess(maxProcess);
+        ObjectAnimator objectAnimator=ObjectAnimator.ofFloat(arcView,"rotationY",50,150,maxProcess);
+        objectAnimator.setDuration(1000*2);
+        objectAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        objectAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float process = (float) animation.getAnimatedValue();
+                arcView.setProcess(process);
+                arcView.setScale(process/maxProcess);
+                arcView.postInvalidate();
+            }
+        });
+        objectAnimator.start();
+    }
+
+    private void testAnminationSet()
+    {
+        AnimatorSet set=new AnimatorSet();
+
+        final float maxProcess=360f;
+        arcView.setMaxProcess(maxProcess);
+        ObjectAnimator objectAnimator=ObjectAnimator.ofFloat(arcView,"rotationY",50,maxProcess);
+        objectAnimator.setDuration(1000*2);
+        objectAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        objectAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float process = (float) animation.getAnimatedValue();
+                arcView.setProcess(process);
+                arcView.setScale(process/maxProcess);
+                arcView.postInvalidate();
+            }
+        });
+
+        ObjectAnimator animatorY=ObjectAnimator.ofFloat(arcView,"rotationX",maxProcess,50);
+        animatorY.setDuration(1000*2);
+        animatorY.setInterpolator(new AccelerateDecelerateInterpolator());
+        animatorY.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float process = (float) animation.getAnimatedValue();
+                arcView.setProcess(process);
+                arcView.setScale(process/maxProcess);
+                arcView.postInvalidate();
+            }
+        });
+        //颜色
+        ObjectAnimator translationUp = ObjectAnimator.ofInt(arcView,
+                "backgroundColor", Color.RED, Color.BLUE, Color.GRAY,
+                Color.GREEN);
+        translationUp.setInterpolator(new DecelerateInterpolator());
+        translationUp.setDuration(1500);
+        translationUp.setRepeatCount(-1);
+        /*
+         * ArgbEvaluator：这种评估者可以用来执行类型之间的插值整数值代表ARGB颜色。
+         * FloatEvaluator：这种评估者可以用来执行浮点值之间的插值。
+         * IntEvaluator：这种评估者可以用来执行类型int值之间的插值。
+         * RectEvaluator：这种评估者可以用来执行类型之间的插值矩形值。
+         *
+         * 由于本例是改变View的backgroundColor属性的背景颜色所以此处使用ArgbEvaluator
+         */
+        translationUp.setEvaluator(new ArgbEvaluator());
+
+        set.play(objectAnimator).before(animatorY).with(translationUp);
+        set.start();
     }
 }
